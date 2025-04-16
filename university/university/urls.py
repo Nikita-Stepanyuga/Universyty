a6aff21cafbf505bfa46fb1bb46f1081747119dc
nkost <nkost1105@gmail.com>
2025-04-16 12:33:48 +0500

    Создание проекта.

--- university/university/urls.py
+++ university/university/urls.py
@@ -15,31 +15,53 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from education import views
 
 urlpatterns = [
-    path('add_discipline/', views.add_discipline, name='add_discipline'),
-    path('group/<int:id>/', views.group_detail, name='group_detail'),
-    path('add_group/', views.add_group, name='add_group'),
-    path('group/', views.group_list, name='group_list'),
-    path('group/<int:id>/edit/', views.group_edit, name='group_edit'),
-    path('students/', views.student_list, name='student_list'),
-    path('delete/student/<int:student_id>/', views.delete_student, name='delete_student'),
-    path('teachers/', views.teacher_list, name='teacher_list'),
-    path('university/<int:id>/', views.university_detail, name='university_detail'),
     path('', views.university_list, name='university_main'),
-    path('university_create_view/', views.create_university, name='university_create_view'),
-    path('add_student/', views.add_student, name='add_student'),
-    path('add_teacher/', views.add_teacher, name='add_teacher'),
-    path('delete/teacher/<int:teacher_id>/', views.delete_teacher, name='delete_teacher'),
-    path('add_faculty/', views.add_faculty, name='add_faculty'),
-    path('faculty/<int:id>/edit/', views.faculty_edit, name='faculty_edit'),
-    path('faculties/', views.faculty_list, name='faculty_list'),
-    path('faculty/<int:id>/', views.faculty_detail, name='faculty_detail'),
-    path('add_department/', views.add_department, name='add_department'),
-    path('departments/', views.department_list, name='department_list'),
-    path('department/<int:id>/', views.department_detail, name='department_detail'),
-    path('department/<int:id>/edit/', views.department_edit, name='department_edit'),
+
+    path('university/', include([
+        path('<int:id>/', views.university_detail, name='university_detail'),
+        path('create/', views.create_university, name='university_create_view'),
+    ])),
+
+    path('faculty/', include([
+        path('', views.faculty_list, name='faculty_list'),
+        path('<int:id>/', views.faculty_detail, name='faculty_detail'),
+        path('<int:id>/edit/', views.faculty_edit, name='faculty_edit'),
+    ])),
+
+    path('department/', include([
+        path('', views.department_list, name='department_list'),
+        path('<int:id>/', views.department_detail, name='department_detail'),
+        path('<int:id>/edit/', views.department_edit, name='department_edit'),
+    ])),
+
+    path('group/', include([
+        path('', views.group_list, name='group_list'),
+        path('<int:id>/', views.group_detail, name='group_detail'),
+        path('<int:id>/edit/', views.group_edit, name='group_edit'),
+    ])),
+
+    path('student/', include([
+        path('', views.student_list, name='student_list'),
+        path('delete/<int:student_id>/', views.delete_student, name='delete_student'),
+    ])),
+
+    path('teacher/', include([
+        path('', views.teacher_list, name='teacher_list'),
+        path('delete/<int:teacher_id>/', views.delete_teacher, name='delete_teacher'),
+    ])),
+
+    path('add/', include([
+        path('faculty/', views.add_faculty, name='add_faculty'),
+        path('department/', views.add_department, name='add_department'),
+        path('group/', views.add_group, name='add_group'),
+        path('student/', views.add_student, name='add_student'),
+        path('teacher/', views.add_teacher, name='add_teacher'),
+        path('discipline/', views.add_discipline, name='add_discipline'),
+    ])),
+
     path('admin/', admin.site.urls),
 ]
